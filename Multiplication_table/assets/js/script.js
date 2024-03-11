8cce55ee0a10a1741a73c263bd5955ffd5859369
bhenmors <shopify.bhen@gmail.com>
2024-03-11 23:40:47 +0800

    Updated Multipy Button

--- Multiplication_table/assets/js/script.js
+++ Multiplication_table/assets/js/script.js
@@ -1,21 +1,6 @@
-// //Nested loop
-// let Quotients = "";
-// for(var row = 1; row <= 5; row++) { // 1 2
-//     Quotients = "";
-//     for(var col = 1; col <= 5; col++) {// 1 2
-//         Quotients += (row * col) + " "; //concatenation || 1 2 3 4 5
-//     }
-//     console.log( Quotients);
-// }
+let Multiply = document.getElementById("btnMultiply");
 
-// for(var x = 10; x >= 0; x--) {
-//     console.log(x);
-// }
-
-
-let Execute = document.getElementById("btnExecute");
-
-Execute.addEventListener('click', function() {
+Multiply.addEventListener('click', function() {
     let RowLimit = document.getElementById("RowLimit").value;
     let ColLimit = document.getElementById("ColLimit").value;
     let Quotients = "";
