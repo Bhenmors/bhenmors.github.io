b6acded7a29cd9178ecea93747ca706e8033bd4b
bhenmors <shopify.bhen@gmail.com>
2024-03-14 09:01:03 +0800

    Updated Sum

--- javascript_activity/assets/js/script.js
+++ javascript_activity/assets/js/script.js
@@ -18,15 +18,24 @@ function remove(index) {
     Employee.splice(index, 1);
     DrawList();
 }
+
+function clrTable(){
+    var Table = document.getElementById("table-list");
+    Employee.length = 0;
+    Table.innerHTML = "";
+    
+}
+
 function DrawList() {
     let List = "";
     let sum = 0;
 
-    for(let x = 0; x < Employee.length; x++)
+    for(let x = 0; x < Employee.length; x++){
+        sum += parseInt(Employee[x][2]);
         List += "<tr><td style='width: 25%;'>" + Employee[x][0] + " </td> " + "<td style='width: 25%;'>" + Employee[x][1] + "</td>" + "<td style='width: 25%;'>" + Employee[x][2] + "</td>" + "<td style='width: 25%;'><button class='btn btn-danger' onclick='remove("+x+")'>Remove</button></td></tr>";
-
+    }
+        List += "<tr><td style='width: 25%;'><td style='width: 25%; font-weight: Bolder;'>" + 'Total' + "<td style='width: 25%;'>" + sum + "</td><td style='width: 25%;'><button class='btn btn-danger' onclick='clrTable()'>Clear Table</button></td></tr>";
         document.getElementById("table-list").innerHTML = List;
-   
 }
 
 
