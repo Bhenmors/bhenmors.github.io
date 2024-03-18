6a180f0540a261e8e7b000e2f1a6e02a7557fbc9
bhenmors <shopify.bhen@gmail.com>
2024-03-19 06:43:07 +0800

    Updated Average

--- jsdom/assets/scripts.js
+++ jsdom/assets/scripts.js
@@ -20,25 +20,28 @@ document.querySelector("#btnAdd").addEventListener("click", function () {
     document.querySelector("li:last-child").remove();
     let listItems = document.querySelectorAll("ul#ul-parent li input");
     let sum = 0;
+    let total = 0;
 
     for (let i = 0; i < listItems.length; i++) {
         
         sum += parseInt(listItems[i].value);
-       
+        total = sum / parseInt(listItems.length);
       }
-      document.getElementById("result").innerHTML = sum;
+      document.getElementById("result").innerHTML = total;
   });
   
 
   document.querySelector("#btnCalculate").addEventListener("click", function () {
     let listItems = document.querySelectorAll("ul#ul-parent li input");
     let sum = 0;
+    let total = 0;
 
     for (let i = 0; i < listItems.length; i++) {
         
         sum += parseInt(listItems[i].value);
+        total = sum / parseInt(listItems.length);
        
       }
-      document.getElementById("result").innerHTML = sum;
+      document.getElementById("result").innerHTML = total;
   });
   
